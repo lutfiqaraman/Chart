e34084507561d757e2800dd9a18492ddc65bb908
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-03 23:16:36 +0300

    edit variables

--- js/barchart.js
+++ js/barchart.js
@@ -1,20 +1,20 @@
-const labels = ['January', 'February', 'March', 'April', 'May', 'June',];
+const chartLabels = ['January', 'February', 'March', 'April', 'May', 'June'];
 
-const data = {
-    labels: labels,
+const chartData = {
+    labels: chartLabels,
     datasets: [{
         label: 'My First dataset',
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
+        data: [35, 10, 5, 2, 20, 30, 45],
     }]
 };
 
 const chartConfig = {
     type: 'bar',
-    data,
+    data: chartData,
     options: {}
 };
 
 let chartID = document.getElementById("barChart");
-let chart = new Chart(chartID, chartConfig);
+let chart   = new Chart(chartID, chartConfig);
