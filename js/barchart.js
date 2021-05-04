84ec68668777c39ac41e7676aa055aeaffd9848d
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-04 22:44:50 +0300

    change dataset

--- js/barchart.js
+++ js/barchart.js
@@ -1,13 +1,21 @@
-const chartLabels = ['January', 'February', 'March', 'April', 'May', 'June'];
+const chartLabels = ['Canada', 'United States', 'United Kingdom', 'Australia', 'New Zealand'];
 
 const chartData = {
     labels: chartLabels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [35, 10, 5, 2, 20, 30, 45],
-    }]
+    datasets: [
+        {
+            label: 'Male',
+            backgroundColor: 'rgb(239,36,18)',
+            borderColor: 'rgb(239, 36, 18)',
+            data: [35, 10, 5, 2, 20]
+        },
+        {
+            label: 'Female',
+            backgroundColor: 'rgb(99,138,255)',
+            borderColor: 'rgb(99,138,255)',
+            data: [25, 15, 9, 8, 30]
+        }
+    ]
 };
 
 const chartConfig = {
