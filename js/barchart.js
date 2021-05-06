2f61218db176853c52926421a48998056ede2056
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-06 18:35:31 +0300

    add russia numbers

--- js/barchart.js
+++ js/barchart.js
@@ -1,4 +1,4 @@
-const chartLabels = ['Canada', 'United States', 'United Kingdom', 'Australia', 'New Zealand'];
+const chartLabels = ['Canada', 'United States', 'United Kingdom', 'Australia', 'New Zealand', 'Russia'];
 
 const chartData = {
     labels: chartLabels,
@@ -7,13 +7,13 @@ const chartData = {
             label: 'Male',
             backgroundColor: 'rgb(239,36,18)',
             borderColor: 'rgb(239, 36, 18)',
-            data: [18818000, 163359000, 33183000, 12775000, 2466000]
+            data: [18818000, 163359000, 33183000, 12775000, 2466000, 66919000]
         },
         {
             label: 'Female',
             backgroundColor: 'rgb(99,138,255)',
             borderColor: 'rgb(99,138,255)',
-            data: [19097000, 166781000, 33975000, 12878000, 2552000]
+            data: [19097000, 166781000, 33975000, 12878000, 2552000, 77460000]
         }
     ]
 };
